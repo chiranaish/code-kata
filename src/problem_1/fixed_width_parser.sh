a31abf13620a8074ba419f4940a3985d98381d6d
chiranaish <chiranjeeviaish@gmail.com>
2024-09-16 13:05:51 -0400

    Create fixed_width_parser.sh

---
+++ src/problem_1/fixed_width_parser.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Ensure the script is executable
+# chmod +x run_problem_1.sh
+
+echo "Running fixed width file parser..."
+python3 fixed_width_parser.py
+echo "Parsing complete. Check output.csv."
